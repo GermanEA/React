d60ba42b334de6280f9ce52a32bc3db8138dd2da
GermanEA <german.estrade@hotmail.com>
2022-04-20 12:22:54 +0200

    Fin Contador

--- contador/src/index.js
+++ contador/src/index.js
@@ -1,3 +1,5 @@
+// React < 18
+/**
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import PrimeraApp from './PrimeraApp'
@@ -10,4 +12,18 @@ const divRoot = document.querySelector('#app');
 
 // ReactDOM.render( <PrimeraApp saludo={ 'Hola Mundo' } />  , divRoot );
 ReactDOM.render( <CounterApp value={ 10 } />  , divRoot );
+*/
 
+// React 18
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+// import PrimeraApp from './PrimeraApp'
+import CounterApp from './CounterApp';
+
+import './index.css';
+
+
+const divRoot = document.querySelector('#app');
+const root = createRoot( divRoot );
+
+root.render( <CounterApp value={ 10 } /> );
